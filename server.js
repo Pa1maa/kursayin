0ea568768424c9e56af016bd4a69e18b25aebee3
Edgar <edgarstepanyan080@gmail.com>
2025-10-07 16:59:49 +0400

    the creation

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const path = require("path")
+
+dotenv.config()
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(express.static(path.join(__dirname, "public")))
+
+const mapRoutes = require("./routes/mapRoutes")
+app.use("/api", mapRoutes)
+
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("MongoDb connected")).catch(err => console.log("MongoDB error: ", err))
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
